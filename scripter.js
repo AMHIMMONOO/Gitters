f1c484a569a58d382608fe81020942d11566eb74
benjaminamonoo <benaw@outmail.com>
2023-11-21 22:02:25 +0000

    added content (a function) to scripter

---
+++ scripter.js
@@ -0,0 +1,4 @@
+function realityCheck() {
+  document.getElementById('emo').style.color = "cyan";
+  document.getElementById('check').innerHTML = "They say coding is not for the weak. Well, i'm not weak😎😂😂"
+}
